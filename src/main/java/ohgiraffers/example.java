b40aaa37eb6ace016d40a693c24bf810b4e91475
parkhayarn <s0224y92@gmail.com>
2024-05-10 10:56:26 +0900

    first commit

---
+++ src/main/java/ohgiraffers/example.java
@@ -0,0 +1,12 @@
+package ohgiraffers;
+
+public class example {
+    public static void main(String[] args) {
+
+        System.out.print("hello world");
+        System.out.print("한글은 안깨져요");
+    }}
+
+
+
+
